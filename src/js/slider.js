eeaf17b20b63a4d1a9435e9dfeb7ad6b85d53247
sergiipoliakov <64687462+sergiipoliakov@users.noreply.github.com>
2020-12-08 09:31:51 +0200

    adda

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,40 +1,35 @@
-// const refs = {
-//     slide1: document.querySelector('[data-slide1]'),
-//     slide2: document.querySelector('[data-slide2]'),
-//     slide3: document.querySelector('[data-slide3]'),
+import 'flickity';
 
+const elem = document.querySelector('.main-carousel');
+const flkty = new Flickity(elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+});
+flkty.on('eventName', listener);
 
+// const sliderEl = document.querySelectorAll('[data-slide]');
+
+// autoSlider();
+// console.log(sliderEl[0].classList);
+
+// let active = false;
+
+// let interval;
+// let curr = 0;
+
+// function autoSlider() {
+//   interval = setTimeout(() => {
+//     sliderEl[curr].classList.add('is-active');
+//     // const expanded = sliderEl.getAttribute('aria-expanded') === 'true' || false;
+
+//     curr = curr === sliderEl.length - 1 ? 0 : curr + 1;
+//     // sliderEl.setAttribute('aria-expanded', !expanded);
+
+//     if (sliderEl[0].classList === 'is-active') {
+//       sliderEl[curr].classList.remove('is-active');
+//     }
+
+//     autoSlider();
+//   }, 500);
 // }
-
-const sliderEl = document.querySelectorAll('[data-slide]');
-
-autoSlider()
-console.log(sliderEl[0].classList)
-
-let active = false;
-
-let interval;
-let curr = 0;
-
-function autoSlider() {
-
-    interval = setTimeout(() => { 
-        sliderEl[curr].classList.add('is-active');
-         const expanded = sliderEl.getAttribute('aria-expanded') === 'true' || false;
-       
-        
-        curr = (curr === sliderEl.length - 1) ? 0 : curr + 1;
-        sliderEl.setAttribute('aria-expanded', !expanded);
-
-        if (sliderEl[0].classList === 'is-active') {
-            
-            sliderEl[curr].classList.remove('is-active');
-        }
-        
-
-    autoSlider()
-        
-    },500)
-       
-    
- }
